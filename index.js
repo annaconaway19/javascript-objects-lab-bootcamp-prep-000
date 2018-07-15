ada7fb09879635fb902be95abfb8042b46817cb3
Anna Conaway <annaconaway19@gmail.com>
2018-07-15 18:36:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ function updateObjectWithKeyAndValue(recipes, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
- var newRecipes = recipes[key] = value;
+ recipes[key] = value;
+  return recipes;
+}
+
+function deleteFromObjectByKey (recipes, key) {
+  var newRecipes = Object.assign({}, recipes)
+  delete newRecipes.key;
   return newRecipes;
+}
+
+function destructivelyDeleteFromObjectByKey (recipes, key) {
+  delete recipes[key];
+  return recipes;
 }
